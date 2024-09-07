585a7efa9b000f7dc02a954fca214a77fcb1381e
Nikita Tkachenko <nickita.tcachenko333@gmail.com>
2024-09-07 22:49:45 +0300

    Update HomeWork.cpp

--- HOME WORK/HomeWork.cpp
+++ HOME WORK/HomeWork.cpp
@@ -1,6 +1,66 @@
 #include <iostream>
 using namespace std;
 
+// Отрисовка ракеты //
+void draw(int size);
+
+// Проверка ответа //
+bool answer_checker(char ch);
+
+// Это проверка размера ракеты //
+bool size_checker(int size);
+
+int main()
+{
+    int rocket_size;
+    char answer;
+
+    do
+    {
+        cout << "\n____ROCKET BUILDING____" << endl;
+        cout << "Enter rocket size (minimum size 3): "; cin >> rocket_size;
+
+        if (size_checker(rocket_size))
+        {
+            draw(rocket_size);
+            break;
+        }
+        else 
+        {
+            cout << "The rocket is too small. Wanna try again? (y/n):\n";cin >> answer;
+        }
+
+    }while(size_checker && answer_checker(answer));
+
+    return 0;
+}
+
+bool size_checker(int size)
+{
+    if (size < 3) return false;
+    return true;
+}
+
+bool answer_checker(char ch)
+{
+     while (ch != 'n')
+    {
+        switch(ch)
+        {
+            case 'y':
+                return true;
+                break;
+            case 'n':
+                return false;
+                break;
+            default:
+                cout << "Wrong answer!" << endl;
+                return false;
+                continue;
+        }
+    }
+}
+
 void draw(int size)
 {
     int FuelTank_size = size * 1.4;
@@ -34,36 +94,4 @@ void draw(int size)
 
     for (int i = 0; i < FuelTank_size; i++) cout << "|  | -- |  |" << endl;
     cout << "\\_/ \\__/ \\_/" << endl;
-}
-
-// Это проверка размера ракеты //
-bool size_checker(int &size)
-{
-    if (size < 3) return false;
-    return true;
-}
-
-int main()
-{
-    int rocket_size;
-    char guess;
-
-    do
-    {
-        cout << "____ROCKET BUILDING____" << endl;
-        cout << "Enter rocket size (minimum size 3): "; cin >> rocket_size;
-
-        if (size_checker(rocket_size))
-        {
-            draw(rocket_size);
-            break;
-        }
-        else 
-        {
-            cout << "The rocket is too small. Wanna try again? (y/n):\n";cin >> guess;
-        }
-
-    }while(size_checker && tolower(guess) != 'n');
-
-    return 0;
 }
